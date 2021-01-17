03ce916eb24bbf5f8fe44dc00008722dd040fc1c
Fanrite <76695059+Fanrite@users.noreply.github.com>
2021-01-17 16:20:55 +0200

    input datat

--- lab11zavd1/Program.cs
+++ lab11zavd1/Program.cs
@@ -3,16 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
 
 namespace lab11zavd1
 {
     class Program
     {
+        static bool Inputdata(ref double p, string buk)
+        {
+            string inp;
+
+         vvod:
+            inp = Interaction.InputBox(buk, "vvedenia", $"{p}");
+            try
+            {
+                p = Convert.ToDouble(inp);
+            }
+            catch (System.FormatException)
+            {
+                if (MessageBox.Show("vi vveli ne 4islo" + "\npovtorit?", "uvaga", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                    goto vvod;
+                else
+                    return false;
+            }
+            return true;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine($"testiy git");
-            Console.WriteLine($"a teper z nouta");
-            Console.ReadKey();
+            string outp = "";
+            double min = 10, max = 40, sum;
+        start:
+            int i = 0, n = 10;
+            double[] arr;
+
+
+            //input min & max 
+            if (!Inputdata(ref min, "vvedit minimalne 4slo:"))
+                return;
+            if (!Inputdata(ref max, "vvedit maxsimalne 4slo:"))
+                return;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
